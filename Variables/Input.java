f9ee2f65e1469b128b8d91883f5372a48e4ff9d8
Nikhil Bhasin <50350404+nikhil-12-04@users.noreply.github.com>
2020-04-22 19:22:21 +0530

    Create Input.java

---
+++ Variables/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		double d=scan.nextDouble();
+		String s=scan.next();
+		char x=s.charAt(0);
+		
+		
+	}
+}
