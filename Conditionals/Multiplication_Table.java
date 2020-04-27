42edb84496f828ee533f37de05ffd0a16f508bba
Nikhil Bhasin <50350404+nikhil-12-04@users.noreply.github.com>
2020-04-27 13:11:55 +0530

    Create Multiplication_Table.java

---
+++ Conditionals/Multiplication_Table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner scan=new Scanner(System.in);
+        int n= scan.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(i*n);
+        }
+
+	}
+}
